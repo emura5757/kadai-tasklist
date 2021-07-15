00d9e73a7f14dd83abff0a829e60095d42b7cc5f
emura <emura@192.168.11.8>
2021-07-15 10:53:29 +0900

    new.jspへのforward追加

--- src/main/java/controllers/NewServlet.java
+++ src/main/java/controllers/NewServlet.java
@@ -1,9 +1,7 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,37 +9,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
-import utils.DBUtil;
 
 @WebServlet("/new")
 public class NewServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
-        em.getTransaction().begin();
+        //CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
 
-        //Taskインスタンス生成
-        Task t = new Task();
+        // インスタンス生成
+        request.setAttribute("tasks", new Task());
 
-        //tの各フィールドにデータを代入
-        String content = "task1";
-        t.setContent(content);
-
-        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-        t.setCreated_at(currentTime);
-        t.setUpdated_at(currentTime);
-
-        //DBに保存
-        em.persist(t);
-        em.getTransaction().commit();
-
-        //IDの値表示
-        response.getWriter().append(Integer.valueOf(t.getId()).toString());
-
-        em.close();
-
-
+        request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp").forward(request, response);
     }
 
 
